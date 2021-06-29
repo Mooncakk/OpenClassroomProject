d6029a01fd515cb7408aa7fb880a2d3d04e57134
Mooncakk <mooncak@protonmail.com>
2021-06-30 01:43:40 +0200

    ajout de definition

--- Projet2.py
+++ Projet2.py
@@ -1,8 +1,13 @@
 
+
+from os import truncate
 import requests
 from bs4 import BeautifulSoup
 from lxml import html
 import csv
+from urllib.parse import urljoin
+
+
 
 response = requests.get("http://books.toscrape.com/catalogue/join_902/index.html")
 url = response.url
@@ -13,17 +18,24 @@ def sel(tag, num=0):
     info = soup.select(tag)
     return info[num]
 
-img_url = sel("img")
+image = urljoin(url, sel('img')['src'])
 
-with open("data_book.csv", "w") as csvfile:
-    csvfile.write("Product_page_url : " + url + "\n" + 
-    "Universal_product_code : " + sel("td").text +"\n" +
-    "Title : " + sel("h1").text + "\n" +
-    "Price_including_tax : " + sel("td", 2).text + "\n" +
-    "Price_excliding_tax : " + sel("td", 3).text + "\n" +
-    "Number_available : " + sel("td", 5).text + "\n" +
-    "Product_description : " + sel("p", 3).text + "\n" +
-    "Category : " + sel("a", 3).text + "\n" +
-    "Review_rating : " + sel("td", 6).text + "\n" +
-    "Image_url : " + str(img_url))
+data = {
+    'Product_page_url': url ,
+    'Universal_product_code' : sel("td").text,
+    'Title' : sel("h1").text,
+    'Price_including_tax' : sel("td", 2).text,
+    'Price_excluding_tax' : sel("td", 3).text,
+    'Number_available' : sel("td", 5).text,
+    'Product_description' : sel("p", 3).text,
+    'Category' : sel("a", 3).text,
+    'Review_rating' : sel("td", 6).text,
+    'Image_url': image
+}
+
+
+with open('data_books.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, delimiter = '\t', fieldnames=data.keys())
+    writer.writeheader()
+    writer.writerow(data)
 
